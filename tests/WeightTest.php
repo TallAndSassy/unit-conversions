7e0aa1750543d9783b744c7df36df353f2d4b69c
jjrohrer <jj.bitbucket@rohrer.org>
2020-09-26 16:40:21 -0400

    kg

---
+++ tests/WeightTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tallandsassy\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tallandsassy\UnitConversions\Weight;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+
+
+    public function test_it_can_go_from_kg_to_lbs_test() {
+
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.4623,$lbs);
+    }
+}
