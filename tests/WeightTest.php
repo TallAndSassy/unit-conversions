f44397bc6e2c4ed7065aa0e8e806447e1129c91e
jjrohrer <jj.bitbucket@rohrer.org>
2020-09-28 14:08:19 -0400

    less breaky

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -18,6 +18,6 @@ class WeightTest extends TestCase
     {
         $lbs = Weight::fromKilograms(100)->toLbs();
         $this->assertEquals(220.4623, $lbs);
-        $this->assertEquals(24, $lbs);
+        
     }
 }
